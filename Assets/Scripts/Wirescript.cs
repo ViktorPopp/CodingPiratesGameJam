91b24008b3876a1fcfd68e3a01227419293fc649
david23234 <david.mikkelsen2011@gmail.com>
2024-11-02 20:16:03 +0100

    Removed Start function

--- Assets/Scripts/Wirescript.cs
+++ Assets/Scripts/Wirescript.cs
@@ -9,12 +9,6 @@ public class Wirescript : MonoBehaviour
     private RaycastHit hit;
     public GameObject wire;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
