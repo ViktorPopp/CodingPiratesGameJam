88182aecbb2cfe6c1f0ebedde323c3f2fbcd14df
david23234 <david.mikkelsen2011@gmail.com>
2024-11-03 01:19:40 +0100

    Works
    Co-Authored-By: Viktor Popp <ViktorPopp.dev@gmail.com>

--- Assets/Scripts/Wirescript.cs
+++ Assets/Scripts/Wirescript.cs
@@ -2,34 +2,39 @@ using UnityEngine;
 
 public class Wirescript : MonoBehaviour
 {
-    public GameObject first;
-    public GameObject second;
-
-    private Ray ray;
-    private RaycastHit hit;
+    [SerializeField] private GameObject first;
+    [SerializeField] private GameObject second;
     public GameObject wire;
-    public Vector3 test;
-    public Vector3 middel;
+
+    [SerializeField] private Ray ray;
+    [SerializeField] private RaycastHit hit;
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("f");
+            ray = GetComponentInParent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+
+
             if (Physics.Raycast(ray, out hit))
             {
                 if (!first)
+                {
                     first = hit.collider.gameObject;
+                }
                 else
                     second = hit.collider.gameObject;
             }
         }
         if (first && second)
-        { 
-            wire = Instantiate(wire, first.transform.position, new Quaternion(0,0,0,0));
+        {
+            wire = Instantiate(wire, first.transform.position, new Quaternion(0, 0, 0, 0));
             //wire.transform.position = first.transform.position;
-            wire.transform.localScale = new Vector3(0.01f,0.01f,0.01f);
+            wire.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
             wire.transform.LookAt(second.transform.position, first.transform.position);
             wire.transform.Rotate(wire.transform.rotation.x, wire.transform.rotation.y, wire.transform.rotation.z, 0f);
             first = null;
